980511fe6d58823099778b6795bf6050553b9f65
Nicholas Dudfield <ndudfield@gmail.com>
2015-06-29 19:40:02 +0700

    Fix seqEqual

--- src/address-codec.js
+++ src/address-codec.js
@@ -3,6 +3,10 @@
 const baseCodec = require('base-x');
 
 function seqEqual(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
       return false;
