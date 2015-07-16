89e8774a3dcbc5d336b9cda1986a5a92f23d6f26
Nicholas Dudfield <ndudfield@gmail.com>
2015-07-16 21:11:47 +0800

    Extract utils module

---
+++ src/utils.js
@@ -0,0 +1,42 @@
+function seqEqual(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// Concatenates args and/or contents of sequence conforming args to an Array.
+function toArray() {
+  const args = arguments;
+  if (args.length === 1 && Array.isArray(args[0])) {
+    return args[0];
+  }
+  const ret = [];
+  for (let i = 0; i < args.length; i++) {
+    const arg = args[i];
+    if (arg.length !== undefined) {
+      for (let j = 0; j < arg.length; j++) {
+        ret.push(arg[j]);
+      }
+    } else {
+      ret.push(arg);
+    }
+  }
+  return ret;
+}
+
+function isSet(o) {
+  return o !== null && o !== undefined;
+}
+
+module.exports = {
+  seqEqual,
+  toArray,
+  isSet
+};
